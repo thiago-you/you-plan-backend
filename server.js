f0b848731e7195ce45ac3e5a924b9391d709faf2
thiago-you <thiago.youx@gmail.com>
2021-04-07 22:58:32 -0300

    update pakacge.json config

---
+++ server.js
@@ -0,0 +1,11 @@
+import { create, router as _router, defaults } from 'json-server';
+
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
+const port = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
